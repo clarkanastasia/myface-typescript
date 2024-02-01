4e9f7589830b2b72969a8c5496c72b6f35f4acb7
Anastasia C <clarkanastasia@outlook.com>
2024-02-01 15:35:11 +0000

    add event listeners

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Add your JavaScript here.
+
+  document.getElementById("backgroundButton").addEventListener("click", () =>{
+  const posts= document.getElementsByClassName("postContainer");
+    for(let i=0; i<posts.length; i++){
+      posts[i].style.background=`rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+    }
+  })
+    document.getElementById("menuButton").addEventListener("click", () =>{
+    const mobileMenu = document.getElementById("mobileMenu");
+    const flexContainer = document.getElementById("flexContainer");
+    mobileMenu.classList.toggle("mobileMenuDisplay");
+    flexContainer.classList.toggle("opacity");
+  })
+    document.getElementById("flexContainer").addEventListener("click", () =>{
+    const mobileMenu = document.getElementById("mobileMenu");
+    const flexContainer = document.getElementById("flexContainer");
+    mobileMenu.classList.remove("mobileMenuDisplay");
+    flexContainer.classList.remove("opacity");
+  })
 });
